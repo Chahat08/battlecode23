0d747dbb2d34f9be17c8279c6ff2ed3dff51c269
Ashwani Rathee <ab669522@gmail.com>
2023-01-15 20:02:34 +0530

    Base-0.1

---
+++ src/examplefuncsplayer/HeadquarterStrategy.java
@@ -0,0 +1,38 @@
+package examplefuncsplayer;
+
+import battlecode.common.*;
+
+import static examplefuncsplayer.RobotPlayer.directions;
+import static examplefuncsplayer.RobotPlayer.rng;
+
+public class HeadquarterStrategy {
+
+    /**
+     * Run a single turn for a Headquarters.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    static void runHeadquarters(RobotController rc) throws GameActionException {
+        // Pick a direction to build in.
+        Direction dir = directions[rng.nextInt(directions.length)];
+        MapLocation newLoc = rc.getLocation().add(dir);
+
+        if (rc.canBuildAnchor(Anchor.STANDARD) && rc.getResourceAmount(ResourceType.ADAMANTIUM) >= 300) {
+            // If we can build an anchor do it!
+            rc.buildAnchor(Anchor.STANDARD);
+            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
+        }
+        if (rng.nextBoolean()) {
+            // Let's try to build a carrier.
+            rc.setIndicatorString("Trying to build a carrier");
+            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+                rc.buildRobot(RobotType.CARRIER, newLoc);
+            }
+        } else {
+            // Let's try to build a launcher.
+            rc.setIndicatorString("Trying to build a launcher");
+            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            }
+        }
+    }
+}
