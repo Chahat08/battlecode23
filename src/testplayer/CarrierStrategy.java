73b1e491ee52a220a6190f28c7fb7a97dfbb02ca
“ashwani-rathee” <ab669522@gmail.com>
2023-01-20 12:55:38 +0530

    carrier strat

--- src/testplayer/CarrierStrategy.java
+++ src/testplayer/CarrierStrategy.java
@@ -10,6 +10,18 @@ import static common.utils.Utils.intToLocation;
 import static common.utils.Utils.locationToInt;
 import static testplayer.RobotPlayer.*;
 
+class MapData{
+    static MapLocation loc;
+    static int fvalue;
+
+    public MapData(MapLocation mapLocation, Integer integer) {
+        loc = mapLocation;
+        fvalue = integer;
+    }
+    int getFvalue(){
+        return fvalue;
+    }
+}
 public class CarrierStrategy {
     static int hqLocationNumber = 1;
 
@@ -17,53 +29,60 @@ public class CarrierStrategy {
     static MapLocation end;
 //    static int[] path;
 
-    static List<MapLocation> openList = new ArrayList<>();
-    static List<MapLocation> closedList = new ArrayList<>();
+    static ArrayList<MapLocation> openList = new ArrayList<>();
+    static ArrayList<MapLocation> closedList = new ArrayList<>();
     // carry resources if near hq, well
     // attack-> throws resources at enemies
     // used to put anchors on sky islands
     // get slower with amount carried
     static void runCarrier(RobotController rc) throws GameActionException {
+        System.out.println("Starting carrier:" + rc.getLocation());
         if(turnCount == 1) {
-            MapLocation start = rc.getLocation();
+            MapLocation start = rc.adjacentLocation(Direction.NORTH);
             end = new MapLocation(1, 1);
             openList.add(start);
         }
-        rc.setIndicatorString("Trying to implement A* boss!!");
-//        System.out.println(openList.size());
-        // h = 1
-        // f = g + h;
-        // g = distance from start
+        System.out.println("Open List:"+openList);
         // cleans up open list to ensure memory issue dont occur
-        if(openList.size() > 10){
-            openList.subList(10, openList.size()).clear();
+        if(openList.size() >= 10){
+            System.out.println("Open list too big, cleaning up");
+            openList.subList(9, openList.size()).clear();
         }
+        System.out.println("Is rc action ready?" + rc.isActionReady());
         // if open list is empty, no point in move forward
-        if(!openList.isEmpty()){
+        if(!openList.isEmpty() && rc.isActionReady()){
             // get current best move
             MapLocation current = openList.get(0);
-//            System.out.println("Current: " + current);
             Direction dir = rc.getLocation().directionTo(current);
+
+            System.out.println("A* Debugg: "+ openList.size()+ "Current loc:" + rc.getLocation()+ "Want to move to:" + current);
+
             if(rc.canMove(dir)){
+                System.out.println("Moving to: " + current);
                 rc.move(dir);
-            }
-            openList.remove(current);
-            closedList.add(current);
-            if(current.equals(end)){
-                // found the path
-                System.out.println("Found the path");
-            }
-            // add currents neighbors to open list
-            MapLocation[] neighbors = rc.getAllLocationsWithinRadiusSquared(current, 1);
-            for(MapLocation neighbor : neighbors){
-                if(!closedList.contains(neighbor)){
-                    if(!openList.contains(neighbor)){
-                        openList.add(neighbor);
+                openList.remove(current);
+                closedList.add(current);
+                if(current.equals(end)){
+                    // found the path
+                    System.out.println("Found the path");
+                }
+                for(Direction d: directions){
+                    MapLocation adj = rc.adjacentLocation(d);
+                    System.out.println("Adding to open list"+ adj);
+                    if(rc.onTheMap(adj)) {
+                        openList.add(adj);
                     }
                 }
+
+                System.out.println("Gonna Dive in F Calculations");
+                System.out.println("Open List:"+openList);
+                // calculate f value n reorder the array
+                fcalc(rc);
             }
-            // calculate f value n reorder the array
-            fcalc(rc, current);
+            else{
+                System.out.println("Cannot move to: " + current);
+                System.out.println("Waiting for turn");
+            }
         }
 
 //        // NEW STUFF
@@ -195,27 +214,47 @@ public class CarrierStrategy {
     }
     // implement a star algorithm to move from start to end target
 
-    static void fcalc(RobotController rc, MapLocation current) throws GameActionException {
+    static void fcalc(RobotController rc) throws GameActionException {
         List<Integer> f = new ArrayList<>();
         for (MapLocation loc : openList) {
             int p = 0;
-            if (rc.canSenseLocation(loc)) {
-                MapInfo mapInfo = rc.senseMapInfo(loc);
-                if (mapInfo.isPassable()) p += 10;
-                if (mapInfo.getMapLocation().distanceSquaredTo(end) < start.distanceSquaredTo(end)) p -= 5;
-                if (mapInfo.hasCloud()) p += 2;
-                if (rc.senseRobotAtLocation(mapInfo.getMapLocation()).getTeam() != rc.getTeam()) p -= 5;
-
+            Boolean test = rc.canSenseLocation(loc);
+            MapInfo mapInfo = rc.senseMapInfo(loc);
+            if ( test && mapInfo.isPassable() && rc.canMove(rc.getLocation().directionTo(loc))) {
+                  p -= loc.distanceSquaredTo(end);
+//                if (mapInfo.isPassable()) p += 5;
+//                if (rc.getLocation().distanceSquaredTo(end) > loc.distanceSquaredTo(end)) p += 10;
+//                if (mapInfo.hasCloud()) p += 2;
+//                if (rc.senseRobotAtLocation(mapInfo.getMapLocation()).getTeam() != rc.getTeam()) p -= 5;
             } else {
-                p -= 1000;
+                p -= 100;
             }
             f.add(p);
         }
-        openList.sort(Comparator.comparingInt(f::indexOf));
-
-        for(int i=0;i<f.size();i++){
-           System.out.println("Current: "+current+" OpenList: "+openList.get(i)+" F: "+f.get(i));
+        System.out.println(openList);
+        System.out.println(f);
+//        openList = openList.sorted(Comparator.comparingInt(f::get));
+//        openList.sort(Comparator.comparingInt(f::indexOf));
+//        Collections.sort(openList, Comparator.comparingInt(f::indexOf));
+//        openList.sort(Comparator.comparingInt(f::get));
+        MapData[] data = new MapData[openList.size()];
+        for(int i = 0; i < openList.size(); i++){
+            data[i] = new MapData(openList.get(i), f.get(i));
+            System.out.println(data[i].loc + " " + data[i].fvalue);
+//            System.out.println(openList.get(i) + " " + f.get(i));
         }
+        Collections.sort(Arrays.asList(data), new Comparator<MapData>() {
+            @Override
+            public int compare(MapData o1, MapData o2) {
+                System.out.println(o1.getFvalue() + " "+ o2.getFvalue());
+                return Integer.compare(o1.getFvalue(), o2.getFvalue());
+            }
+        });
+//        Collections.sort(data, (MapData a1, MapData a2) -> a1.fvalue -a2.fvalue);
+//        Collections.sort(data, Comparator.comparing(MapData::getFvalue));
+        System.out.println("After sort:"+ openList);
+//        Collections.reverse(openList);
 
+        System.out.println("We reached here dw!!");
     }
 }
